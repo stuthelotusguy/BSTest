bc854d699ad276ff9de9dcc1583f53c2734ac4d8
Stuart Russell <stuart@youilabs.com>
2016-04-18 13:03:24 -0400

    Script removes old package to clean unused assets.

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
-zip -r BSTest.zip images source fonts channelstore components assets* -x *.gitignore
+#!/bin/bash
+
+if [ -f BSTest.zip ] ; then
+    rm BSTest.zip
+fi
+zip -r BSTest.zip manifest images source fonts channelstore components assets* -x *.gitignore
 curl --user "rokudev:youi" --digest -v -S -F "mysubmit=Install" -F "archive=@BSTest.zip" -F "passwd=" http://"$1"/plugin_install | grep "<font color" | sed "s/<font color=\"red\">//"
