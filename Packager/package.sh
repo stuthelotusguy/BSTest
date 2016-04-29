88397c9abc7468b2a3351f97eee2c24b8689bb48
Stuart Russell <stuart@youilabs.com>
2016-04-29 15:40:46 -0400

    Package updates and references.

--- Packager/package.sh
+++ Packager/package.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [ -f "$1".zip ] ; then
-    rm "$1".zip
+if [ -f BSTestLib.zip ] ; then
+    rm BSTestLib.zip
 fi
-zip -r "$1".zip manifest source components -x *.gitignore
+zip -r BSTestLib.zip manifest source components -x *.gitignore
 
-curl --user "rokudev:youi" --digest -v -S -F "mysubmit=Install" -F "archive=@"$1".zip" -F "passwd=" http://"$2"/plugin_install | grep "<font color" | sed "s/<font color=\"red\">//"
+curl --user "rokudev:youi" --digest -v -S -F "mysubmit=Install" -F "archive=@BSTestLib.zip" -F "passwd=" http://"$1"/plugin_install | grep "<font color" | sed "s/<font color=\"red\">//"
 
-#wget --user rokudev --password youi -F "app_name="$1"" -F "passwd=CccchAmCIYZJHtbL4KH/Vg==" http://"$2"/plugin_package
+#wget --user rokudev --password youi -F "app_name=BSTestLib" -F "passwd=CccchAmCIYZJHtbL4KH/Vg==" http://"$1"/plugin_package
 
-curl -d --user "rokudev:youi" --digest -v -S -F "app_name="$1"" -F "passwd=CccchAmCIYZJHtbL4KH/Vg==" http://"$2"/plugin_package > text.txt
+curl -d --user "rokudev:youi" --digest -v -S -F "app_name=BSTestLib" -F "passwd=CccchAmCIYZJHtbL4KH/Vg==" http://"$1"/plugin_package
 
-#curl --user "rokudev:youi" --digest -v -S -F "app_name="$1"" -F "passwd=CccchAmCIYZJHtbL4KH/Vg==" http://"$2"/plugin_package > text.txt
+#curl --user "rokudev:youi" --digest -v -S -F "app_name=BSTestLib" -F "passwd=CccchAmCIYZJHtbL4KH/Vg==" http://"$1"/plugin_package
