36ef08a3fc0c1a38f3ebf3e85891f9de3ab974a1
Mathieu-Andre Chiasson <matt.chiasson@youi.tv>
2016-10-17 17:52:49 -0400

    run.sh can now be used in windows 10 (using mingw provided by git)

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 if [ -f BSTest.zip ] ; then
     rm BSTest.zip
 fi
-zip -r BSTest.zip manifest images source components -x *.gitignore
+cmake -E tar "cfv" "BSTest.zip" --format=zip manifest images source components
 curl --user "rokudev:youi" --digest -v -S -F "mysubmit=Install" -F "archive=@BSTest.zip" -F "passwd=" http://"$1"/plugin_install | grep "<font color" | sed "s/<font color=\"red\">//"
